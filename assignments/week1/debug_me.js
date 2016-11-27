dc8dd5101fd201591caa49ee1b7da62b5cf4b79c
Henry Lucco <hlucco@gmail.com>
2016-11-26 20:32:38 -0800

    Cool Example!

---
+++ assignments/week1/debug_me.js
@@ -0,0 +1,7 @@
+function add10(n) {
+    return "\n Name: " + n.name + " \n Age: " + n.age + " \n Phone: " + n.phone + " \n Car: " + n.car;
+}
+var y = { name: 'Henry', age: 16, phone: 'Galaxy S7', car: 'VW Jetta' };
+var z = 2;
+console.log(add10(y));
+//# sourceMappingURL=debug_me.js.map
